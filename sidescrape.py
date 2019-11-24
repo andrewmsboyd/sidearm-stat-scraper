d0e712ec9c28bd191d8810436cb18176f028866c
Andy Boyd <amsb@pm.me>
2019-11-24 12:14:48 -0600

    added ability for the user to specify absolute path where the output file should be saved

--- sidescrape.py
+++ sidescrape.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 #URL = 'https://tarletonsports.com/sports/baseball/stats/2019'
 url = input('\nEnter the URL from which to scrape the statistical data\n: ')
 
+#ask the user to specify a directory for the output data file
+path = input('\nEnter the absolute path where you would like to save the data set\n:')
+
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/77.0.3865.90 Safari/537.36'}
 
 page = requests.get(url, headers=headers)
@@ -14,9 +17,6 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # the following two lines only pull out the header row from the table on the page
 # stat_table = soup.find('thead')
 # stat_data = stat_table.find_all('tr')
-
-
-#ask the user to specify a directory for the output data file
 
 
 #scrape the stat data and dump it to a file in the same directory
