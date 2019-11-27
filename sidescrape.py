b9ecfb0ae6f2da25d02bf8493c7d71fd9b27a2cb
Andy Boyd <amsb@pm.me>
2019-11-27 12:49:54 -0600

    fixed formatting with vs code extension

--- sidescrape.py
+++ sidescrape.py
@@ -5,10 +5,12 @@ from bs4 import BeautifulSoup
 #URL = 'https://tarletonsports.com/sports/baseball/stats/2019'
 url = input('\nEnter the URL from which to scrape the statistical data\n: ')
 
-#ask the user to specify a directory for the output data file
-path = input('\nEnter the absolute path where you would like to save the data set\n:')
+# ask the user to specify a directory for the output data file
+path = input(
+    '\nEnter the absolute path where you would like to save the data set\n:')
 
-headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/77.0.3865.90 Safari/537.36'}
+headers = {
+    "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/77.0.3865.90 Safari/537.36'}
 
 page = requests.get(url, headers=headers)
 
@@ -19,7 +21,7 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # stat_data = stat_table.find_all('tr')
 
 
-#scrape the stat data and dump it to a file in the same directory
+# scrape the stat data and dump it to a file in the same directory
 stat_table = soup.find('tbody')
 stat_data = stat_table.find_all('tr')
 
